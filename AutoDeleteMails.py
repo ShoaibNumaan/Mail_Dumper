8a1d8dd14b79e1243a43cf3de378f6630fe33753
Numaan <shoaib.numaan@gmail.com>
2021-10-16 19:15:35 +0530

    Fixed name error

--- AutoDeleteMails.py
+++ AutoDeleteMails.py
@@ -1,39 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
 import pyautogui as p
 from time import sleep
 
-#
+
 def del_Old_Mail(num):
+    print("----Started----")
     for x in range(num):
         if x == 0:
             sleep(5)
             continue
+        print(f"Processed {x} clicks")
         p.moveTo(280,200, duration =0.5)
         p.click()
         sleep(0.5)
         p.moveRel(165,0, duration=0.5)
         p.click()
         sleep(5)
+    print("----Finished----")
+
 
 # main function
 def main():
-    main_count = int(input("Number of e-mails to delete: "))
+    mail_count = int(input("Number of e-mails to delete: "))
     mails_per_page = int(input("Enter the e-mails visible per page: "))
     clicks = mail_count // mails_per_page
-    print(f"Number of Clicks: {clicks}")
     del_Old_Mail(clicks)
 
 
 main()
-
-
-# In[ ]:
-
-
-
-
